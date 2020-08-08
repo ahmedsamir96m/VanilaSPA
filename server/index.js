05ecdf7193226339239418a3cf37c7a06fe310b3
Ahmad Samir <ahmedsamir96m@gmail.com>
2020-08-08 17:01:03 +0200

    undo the last commit, changed it back to the way it was

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.get("/", (req, res) =>
 
 app.post("/video-request", async (req, res, next) => {
   const response = await VideoRequestData.createRequest(req.body);
-  res.redirect("http://127.0.0.1:5500/");
+  res.send(response);
   next();
 });
 
